fadcff49200578a097f26fb461a6f71c8183df9e
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-24 23:41:51 -0400

    start work on systems extension

--- src/simplecs/systems.clj
+++ src/simplecs/systems.clj
@@ -1,1 +1,24 @@
 (ns simplecs.systems)
+
+; systems builds on the core, rather than being part of it
+; it is optional and experimental
+
+; PIPELINES
+
+; a pipeline is:
+; a collection of systems,
+; with a specified order
+; ideally, this should incorporate data-oriented design.
+; failing that, liberal use of -> and ->>.
+
+(defn create-pipeline
+  "Create a data processing pipeline."
+  []
+  []) ; uhhhh
+
+; SYSTEMS
+
+; what is a system?
+; a system is a function that operates on a game state
+; aiming to blahhhhhh.
+; okay, i need a better definition of them.
